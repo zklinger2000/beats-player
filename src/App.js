834c408685718355b7bff9a5b84ad52f736a8221
Zack Klinger <zack@zklinger.com>
2019-06-14 22:49:27 -0700

    Add two separate BeatsPlayer components to the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import BeatsPlayer from './BeatsPlayer';
+import AudioVisualiser from './AudioVisualiser';
+import DemoViz from './DemoViz';
 import './App.css';
 
 
@@ -7,7 +9,22 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BeatsPlayer/>
+        <section>
+          <h1>Web Audio API and HTML Canvas</h1>
+          <BeatsPlayer>
+            <AudioVisualiser width="600px" height="300px"/>
+          </BeatsPlayer>
+          <p>
+
+          </p>
+        </section>
+
+        <section>
+          <BeatsPlayer>
+            <DemoViz width="300px" height="300px"/>
+          </BeatsPlayer>
+        </section>
+        
       </div>
     );
   }
